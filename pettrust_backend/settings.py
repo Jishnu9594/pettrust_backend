5960fd203b83ae50983d0f8a1ad3808353095fa6
Jishnu9594 <jishnuaswin025@gmail.com>
2024-12-10 17:06:54 +0530

    Initial commit for Heroku deployment

--- pettrust_backend/settings.py
+++ pettrust_backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "django-insecure-k^yjjia37a)butad%45^ra)&cttj(uwgbyvd^zqf_&qlctf@$t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pettrust-backend-1wzw.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
